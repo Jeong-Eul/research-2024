812ad49770dcbbf98c0da33a10fc1d1324e407ed
Jeong-Eul <wjddmf0321@naver.com>
2024-09-30 21:00:27 +0900

    scripts

--- Replicate_for_P19/code/main_scripts_japan.sh
+++ Replicate_for_P19/code/main_scripts_japan.sh
@@ -1,35 +1,44 @@
 export TOKENIZERS_PARALLELISM=false
 export CUDA_VISIBLE_DEVICES=0,1
+export NCCL_DEBUG=INFO
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
-model_name=EHRTimeLLM_Reprogramming_visualization
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
 
+model_name=EHRTimeLLM
+train_epochs=20
+learning_rate=0.0001
 llama_layers=16
 
-master_port=0
+master_port=1041
 num_process=2
-batch_size=48
+batch_size=4
 d_model=16
-d_ff=16
+d_ff=32
 
 comment='EHRTimeLLM'
 
-accelerate launch --config_file /home/DAHS2/.cache/huggingface/accelerate/default_config.yaml --num_processes 2 inf_EHRTimeLLM_v3.py \
+accelerate launch --config_file /home/DAHS2/.cache/huggingface/accelerate/default_config.yaml --num_processes 2 run_EHRTimeLLM_IME.py \
   --is_training 1 \
   --root_path /home/DAHS2/Timellm/Replicate_for_P19/ \
   --PT_dict_path dataset/data/processed_data/PT_dict_list_6.npy \
   --outcomes_path dataset/data/processed_data/arr_outcomes_6.npy \
-  --trained_model_path code/checkpoints/Sepsis_EHRTimeLLM_P19_60_pl1_dm16_nh1_df16_il_0-EHRTimeLLM/checkpoint_split_1 \
-  --split_num 1 \
+  --split_num 4 \
+  --mode Train \
   --model_id Sepsis \
   --model $model_name \
   --data P19 \
-  --mode Visualization \
+  --lradj PEMS \
   --seq_len 60 \
   --pred_len 1 \
-  --n_heads 1 \
+  --n_heads 8 \
+  --itr 5 \
   --llm_model GPT2 \
   --d_model $d_model \
   --d_ff $d_ff \
   --batch_size $batch_size \
+  --learning_rate $learning_rate \
   --llm_layers $llama_layers \
+  --train_epochs $train_epochs \
   --model_comment $comment 
